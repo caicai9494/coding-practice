3495120d335e10e644ed00263576237f922b052b
caicai9494 <cailinzhe@gmail.com>
2016-03-27 23:32:38 -0400

    added skeleton code

---
+++ mit-algo/tmp/template.cc
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <cassert>
+#include <limits>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "lz_error.h"
+
+using namespace std;
+
+class Solution {
+  public:
+};
+
+int main()
+{
+    Solution s;
+    //int A[] = {5, 7, 7, 8, 8, 10};
+    //vector<int> num = {4, 5, 6, 7 , 8, 0, 1, 2};
+    //cout << s. << endl;
+    return 0;
+}
